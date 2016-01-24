6335529d4a6973c625b829597cd43357b40c86a0
rkfg <rkfg@rkfg.me>
2016-01-24 20:54:31 +0300

    Add release script.

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+for os in linux32 linux64
+do
+  mvn clean package -P$os
+  TARGET_DIR="release/$os"
+  mkdir -p "$TARGET_DIR"
+  TARGET_JAR="$TARGET_DIR/pfe-gui.jar"
+  cp "target/pfe-$os.jar" "$TARGET_JAR"
+  chmod +x "$TARGET_JAR"
+  cp "pfe_settings.ini" "$TARGET_DIR"
+done
